e04d273d9aecddb47eb1c6add671771717e90edc
Amandeep Yadav <amandeepyadav0704@gmail.com>
2026-01-20 00:59:01 +0530

    new commits

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = ({Imgsrc,index},props) => {
   return (
     <>
     <div value={Imgsrc} className="h-[25vh] bg-cover bg-white w-[10vw] rounded-3xl text-red shadow-[10px_0_10px_-2px_rgba(0,0,0,0.5),-10px_0_10px_-2px_rgba(0,0,0,0.5)]">
-    <img src={Imgsrc} key={index} alt="Hero image" className="w-full h-64 object-cover rounded-xl" />      
+    <img src={Imgsrc} key={index} alt="Hero image" className="w-full h-full object-cover rounded-xl mb-5" />      
     </div>
     </>
   )
